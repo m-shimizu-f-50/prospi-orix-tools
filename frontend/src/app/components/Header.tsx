a66900f99d543d2f913e91ce5e13317c4c592f5c
simizu-masato <m.shimizu@trigger4.com>
2025-06-27 21:43:39 +0900

    ヘッダーコンポーネント作成

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -1,30 +1,36 @@
 import Link from 'next/link';
 
 export default function Header() {
+	const navList = [
+		{
+			url: '/',
+			title: 'トップ',
+		},
+		{
+			url: '/players',
+			title: '選手一覧',
+		},
+	];
+
 	return (
 		<header className='border-b shadow-sm'>
+			{/* タイトル */}
 			<div className='px-2 py-5'>
 				<h1 className='text-lg font-semibold'>プロスピA | オリックス純正</h1>
 			</div>
 			{/* 下部：メインメニュー */}
 			<nav className='bg-white border-t border-gray-200'>
 				<ul className='flex px-4 pt-2 space-x-4 font-semibold text-sm overflow-x-auto'>
-					<li>
-						<Link
-							href='/'
-							className="relative inline-block px-4 pb-2 hover:after:content-[''] hover:after:absolute hover:after:left-1/2 hover:after:translate-x-[-50%] hover:after:bottom-0 hover:after:h-[4px] hover:after:w-full hover:after:bg-yellow-400"
-						>
-							トップ
-						</Link>
-					</li>
-					<li>
-						<Link
-							href='/players'
-							className="relative inline-block px-4 pb-2 hover:after:content-[''] hover:after:absolute hover:after:left-1/2 hover:after:translate-x-[-50%] hover:after:bottom-0 hover:after:h-[4px] hover:after:w-full hover:after:bg-yellow-400"
-						>
-							選手一覧
-						</Link>
-					</li>
+					{navList.map((nav) => (
+						<li key={nav.title}>
+							<Link
+								href={nav.url}
+								className="relative inline-block px-4 pb-2 hover:after:content-[''] hover:after:absolute hover:after:left-1/2 hover:after:translate-x-[-50%] hover:after:bottom-0 hover:after:h-[4px] hover:after:w-full hover:after:bg-yellow-400"
+							>
+								{nav.title}
+							</Link>
+						</li>
+					))}
 				</ul>
 			</nav>
 		</header>
