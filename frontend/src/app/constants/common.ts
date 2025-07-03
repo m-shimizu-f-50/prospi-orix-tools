0a0ab74cb35aa5b798cf273b6c8173c23d9ea31a
simizu-masato <m.shimizu@trigger4.com>
2025-07-03 21:50:26 +0900

    超特能定義

--- frontend/src/app/constants/common.ts
+++ frontend/src/app/constants/common.ts
@@ -29,3 +29,32 @@ export const TRAJECTORIES = [
 	{ value: '6', label: 'アーチスト' },
 	{ value: '7', label: 'ラインドライブ' },
 ];
+
+// スキル定義
+export const PITCHER_SUPER_SKILLS = [
+	{ value: '1', label: '超ノビ' },
+	{ value: '2', label: '超キレ' },
+	{ value: '3', label: '超援護' },
+	{ value: '4', label: '超奪三振' },
+	{ value: '5', label: '超コントロール' },
+	{ value: '6', label: '超対ピンチ' },
+	{ value: '7', label: '超豪速球' },
+	{ value: '8', label: '超重い球' },
+	{ value: '9', label: '超存在感' },
+	{ value: '10', label: '超打たれ強さ' },
+	{ value: '10', label: '超対左打者' },
+	{ value: '11', label: '超対右打者' },
+	{ value: '12', label: '超奪三振ショー' },
+	{ value: '13', label: '超緩急' },
+	{ value: '14', label: '超グランドボーラー' },
+	{ value: '15', label: '超奥行き' },
+];
+
+export const PITCHER_SKILLS = [
+	'広角打法',
+	'アーチスト',
+	'対ピンチ',
+	'選球眼',
+	'固め打ち',
+	// など...
+];
